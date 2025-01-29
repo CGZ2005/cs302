1ec2d56a653f99599ba08d1a389373ea4b03fc69
Carlos Gomez Zevallos <cgomezze@hydra11.eecs.utk.edu>
2025-01-29 14:02:06 -0500

    updating challenge 2

---
+++ challenges/challenge02/solution.cpp
@@ -0,0 +1,21 @@
+// Challenge 02: Closest Numbers
+// Name:  Carlos Gomez: cgomezze
+
+// Brief description:
+
+// This code solves yyyy based on the following idea/implementation...
+
+#include <algorithm>
+#include <climits>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Main Execution
+
+int main(int argc, char *argv[]) {
+  // hi :)
+  return EXIT_SUCCESS;
+}
