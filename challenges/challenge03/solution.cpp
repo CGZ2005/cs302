22fb90d4fec84510d56075df565c5cc0a38a4bd5
Carlos Gomez Zevallos <cgomezze@hydra11.eecs.utk.edu>
2025-02-03 20:51:42 -0500

    did main, need function

---
+++ challenges/challenge03/solution.cpp
@@ -0,0 +1,24 @@
+// Challenge 03: Palindrome Permutation
+//
+//
+//
+//
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+boolean is_palindrome(const string &s) {
+  unordered_map<char, int> umap;
+  int odd_count;
+}
+int main(int argc, char *argv[]) {
+  string s;
+  while (cin >> s) {
+    if (is_palindrome(s)) {
+      cout << "..." << endl;
+    } else {
+      cout << "..." << endl;
+    }
+  }
+  return (0);
+}
